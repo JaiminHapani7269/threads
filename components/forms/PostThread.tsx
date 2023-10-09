c8f550f60eca2897c475dbc746044a37cba835bd
Jaimin Hapani <jaiminhapani7787@gmail.com>
2023-10-09 16:18:44 +0530

    "added to git"

---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,83 @@
+"use client";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Button } from "../ui/button";
+import { Textarea } from "../ui/textarea";
+import { usePathname, useRouter } from "next/navigation";
+import { ThreadValidation } from "@/lib/validation/thread";
+import { createThread } from "@/lib/actions/thread.actions";
+// import { updateUser } from "@/lib/actions/user.action";
+
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+  userId: string;
+}
+
+function PostThread({ userId }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+    await createThread({
+      text: values.thread,
+      author: userId,
+      communityId: null,
+      path: pathname,
+    });
+    router.push("/");
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col justify-start mt-10 gap-10">
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex flex-col w-full gap-3 ">
+              <FormLabel className="text-base-semibold text-light-3">
+                Content
+              </FormLabel>
+              <FormControl className="no-focus border border-dark-4 bg-dark-4 text-light-1">
+                <Textarea rows={12} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="bg-primary-500">
+          Post Thread
+        </Button>
+      </form>
+    </Form>
+  );
+}
+export default PostThread;
